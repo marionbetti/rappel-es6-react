27e4586cecdbc239da407398f6a61fc32ae137ad
malik <malik.h@webdevpro.net>
2019-10-16 11:45:15 +0200

    plusieurs manières de définir des méthodes dans un objet et plusieurs manières d'appeler des méthodes d'objet

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 // dans le dossier react => create-react-app jour1-react-es6 (télécharger react)
-// cd jour1-react-es6 
+// cd jour1-react-es6
 // code . => ouvrir visual studio
 // src/index.js => coller tout le texte que l'on avait mis dans jour1-es6/01-objet.js qui remplace tout le texte
 // yarn start
-
 
 // objet littéral
 // variable complexe : contient plusieurs valeurs
@@ -11,10 +10,16 @@
 
 const person = {
   nom: "Moi", // attribut d'objet ou propriété d'objet
-  parler: function() {}, // méthode d'objet = fonction dans l'objet manière classique d'écrire
+  parler: function() {
+    console.log("parler");
+  }, // méthode d'objet = fonction dans l'objet manière classique d'écrire
   marcher() {
     console.log("Marcher");
   } // nouvelle manière d'écrire une méthode de d'objet
 };
 
+// deux manières d'utiliser de méthode
 person.marcher();
+
+const action = "parler";
+person[action](); //autre manière d'exécuter les instructions de person.marcher
